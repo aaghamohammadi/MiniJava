89f816b6854b82ee985215dace64b9e7ce2674f2
alireza <al.aghamohammadi@gmail.com>
2015-08-27 19:32:56 +0430

    finished

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+import parser.Parser;
+import scanner.lexicalAnalyzer;
+import scanner.token.Token;
+import scanner.type.Type;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Parser parser = new Parser();
+        try {
+            parser.startParse(new Scanner(new File("src//resource//code")));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
+
+
+
