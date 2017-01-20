c6132e542ed823a68fc7882f5ef2b10d0ab3170c
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2017-01-20 12:45:12 +0330

    add action

--- src/main/java/parser/Action.java
+++ src/main/java/parser/Action.java
@@ -4,30 +4,31 @@ package parser;
  * Created by mohammad hosein on 6/25/2015.
  */
 public class Action {
-    public act action;
-    //if action = shift : number is state
-    //if action = reduce : number is number of rule
-    public int number;
+  public act action;
+  //if action = shift : number is state
+  //if action = reduce : number is number of rule
+  public int number;
 
-    public Action(act action , int number){
-        this.action =action;
-        this.number = number;
+  public Action(act action, int number) {
+    this.action = action;
+    this.number = number;
+  }
+
+  public String toString() {
+    switch (action) {
+      case accept:
+        return "acc";
+      case shift:
+        return "s" + number;
+      case reduce:
+        return "r" + number;
     }
-    public String toString(){
-        switch (action)
-        {
-            case accept:
-                return "acc";
-            case shift:
-                return "s"+number;
-            case reduce:
-                return "r"+number;
-        }
-        return action.toString()+number;
-    }
+    return action.toString() + number;
+  }
 }
-enum act{
-    shift,
-    reduce,
-    accept
+
+enum act {
+  shift,
+  reduce,
+  accept
 }
